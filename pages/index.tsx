eefd70650d33bc3ad38d5769b23a3dcc428a1283
Dian Rahmaji <dianrahmaji@gmail.com>
2022-09-11 11:43:43 +0700

    feat: added text

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <pre className="text-base">Hi, Mom!</pre>;
+  return (
+    <div className="h-screen max-w-7xl mx-auto justify-center flex flex-col min-w-[500px]">
+      <div className="text-center uppercase font-bold">
+        <p className="text-lg md:text-xl lg:text-2xl xl:text-3xl font-mono">
+          Countdown Penarikan
+        </p>
+        <p className="text-[#00a8cc] text-xl md:text-2xl lg:text-3xl xl:text-4xl font-sans">
+          KKN-PPM UGM
+        </p>
+        <p className="text-lg md:text-xl lg:text-2xl xl:text-3xl font-mono">
+          Periode 2 Tahun 2021
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
