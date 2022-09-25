873f96993966c33a6931c8b05a4070621deae52a
Dian Rahmaji <dianrahmaji@gmail.com>
2022-09-25 13:09:44 +0700

    chore: add api/graphql.ts

---
+++ pages/api/graphql.ts
@@ -0,0 +1,33 @@
+import { ApolloServer } from 'apollo-server-micro'
+import Cors from 'micro-cors'
+
+import type { NextConfig } from 'next'
+
+import { typeDefs } from '../../graphql/schema'
+import { resolvers } from '../../graphql/resolvers'
+
+const cors = Cors()
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers })
+
+const startServer = apolloServer.start()
+
+// TODO: Add types
+export default cors(async function handler(req, res) {
+  if (req.method === 'OPTIONS') {
+    res.end()
+    return false
+  }
+
+  await startServer
+
+  await apolloServer.createHandler({
+    path: '/api/graphql',
+  })(req, res)
+})
+
+export const config: NextConfig = {
+  api: {
+    bodyParser: false,
+  }
+}
