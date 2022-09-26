aae2a0fdc8af82af44794ea76a75bc8adcd15225
Dian Rahmaji <dianrahmaji@gmail.com>
2022-09-26 15:15:34 +0700

    chore: remove ApolloProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,16 @@
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
-import { ApolloProvider } from "@apollo/client";
 
 import "../styles/globals.css";
-import apolloClient from "../lib/apollo";
 
 import Header from "../components/header";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ApolloProvider client={apolloClient}>
-      <ThemeProvider attribute="class">
-        <Header />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </ApolloProvider>
+    <ThemeProvider attribute="class">
+      <Header />
+      <Component {...pageProps} />
+    </ThemeProvider>
   );
 }
 
