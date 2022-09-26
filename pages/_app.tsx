e93aeaa6eb211b673216cd0c52613b219879c60c
Dian Rahmaji <dianrahmaji@gmail.com>
2022-09-26 13:12:35 +0700

    feat: add @apollo/client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,20 @@
-import "../styles/globals.css";
-
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import { ApolloProvider } from "@apollo/client";
+
+import "../styles/globals.css";
+import apolloClient from "../lib/apollo";
 
 import Header from "../components/header";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class">
-      <Header />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <ApolloProvider client={apolloClient}>
+      <ThemeProvider attribute="class">
+        <Header />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
 
