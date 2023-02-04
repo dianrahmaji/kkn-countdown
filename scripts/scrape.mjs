586f373d01c7598dce8a8d1b5010a60161d49c18
Dian Rahmaji <dianrahmaji@gmail.com>
2023-02-04 07:41:30 +0700

    add date and period parser with regex

--- scripts/scrape.mjs
+++ scripts/scrape.mjs
@@ -6,10 +6,23 @@ const body = await response.text();
 
 const $ = cheerio.load(body);
 
+const title = $(".post-title h3").first().text().trim();
+
+const periods = title.match(/(\d+)/g).map((v) => parseInt(v));
+
+//TODO: Check periods in DB
+console.log(periods);
+
 const content = $(".entry-content .ecae p")
   .map(function () {
     return $(this).text();
   })
   .get()[2];
 
-console.log(content);
+//TODO: Fix Invalid Date
+const dates = content
+  .match(/(\d{1,2}) (\w+) (\d{4})/g)
+  .map((date) => new Date(date));
+
+//TODO: Insert to DB
+console.log(dates);
